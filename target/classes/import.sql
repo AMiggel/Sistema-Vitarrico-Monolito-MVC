450ff489608f867ef72a434cab832d0f18c7f21d
Antonio Miguel Marín <antonio.marin@ceiba.com.co>
2019-11-14 13:12:53 -0500

    target

---
+++ target/classes/import.sql
@@ -0,0 +1,25 @@
+/* Populate tables */
+INSERT INTO clientes (nombre, apellido, email, create_at, direccion, ciudad) VALUES('Antonio', 'Marín', 'antonio@gmail.com', '2017-08-01','CRA 80 20-65','Medellín');
+INSERT INTO clientes (nombre, apellido, email, create_at, direccion, ciudad) VALUES('John', 'Doe', 'john.doe@gmail.com', '2017-08-02','CRA 80 20-65','Envigado');
+INSERT INTO clientes (nombre, apellido, email, create_at, direccion, ciudad) VALUES('Linus', 'Torvalds', 'linus.torvalds@gmail.com', '2017-08-03','CRA 80 20-65','Bello');
+INSERT INTO clientes (nombre, apellido, email, create_at, direccion, ciudad) VALUES('Jane', 'Doe', 'jane.doe@gmail.com', '2017-08-04','CRA 80 ,20-65','Itagui');
+INSERT INTO clientes (nombre, apellido, email, create_at, direccion, ciudad) VALUES('Rasmus', 'Lerdorf', 'rasmus.lerdorf@gmail.com', '2017-08-05','CRA 80 20-65','Sabaneta');
+INSERT INTO clientes (nombre, apellido, email, create_at, direccion, ciudad) VALUES('Erich', 'Gamma', 'erich.gamma@gmail.com', '2017-08-06','CRA 80 20-65','Medellín');
+INSERT INTO clientes (nombre, apellido, email, create_at, direccion, ciudad) VALUES('Richard', 'Helm', 'richard.helm@gmail.com', '2017-08-07','CRA 80 20-65','Medellín');
+INSERT INTO clientes (nombre, apellido, email, create_at, direccion, ciudad) VALUES('Ralph', 'Johnson', 'ralph.johnson@gmail.com', '2017-08-08','CRA 80 20-65','Medellín');
+INSERT INTO clientes (nombre, apellido, email, create_at, direccion, ciudad) VALUES('John', 'Vlissides', 'john.vlissides@gmail.com', '2017-08-09','CRA 80 20-65','Medellín');
+INSERT INTO clientes (nombre, apellido, email, create_at, direccion, ciudad) VALUES('James', 'Gosling', 'james.gosling@gmail.com', '2017-08-010','CRA 80 20-65','Medellín');
+INSERT INTO clientes (nombre, apellido, email, create_at, direccion, ciudad) VALUES('Bruce', 'Lee', 'bruce.lee@gmail.com', '2017-08-11','CRA 80 20-65','Medellín');
+INSERT INTO clientes (nombre, apellido, email, create_at, direccion, ciudad) VALUES('Johnny', 'Doe', 'johnny.doe@gmail.com', '2017-08-12','CRA 80 20-65','Medellín');
+INSERT INTO clientes (nombre, apellido, email, create_at, direccion, ciudad) VALUES('John', 'Roe', 'john.roe@gmail.com', '2017-08-13','CRA 80 20-65','Medellín');
+INSERT INTO clientes (nombre, apellido, email, create_at, direccion, ciudad) VALUES('Jane', 'Roe', 'jane.roe@gmail.com', '2017-08-14','CRA 80 20-65','Medellín');
+INSERT INTO clientes (nombre, apellido, email, create_at, direccion, ciudad) VALUES('Richard', 'Doe', 'richard.doe@gmail.com', '2017-08-15','CRA 80 20-65','Medellín');
+
+INSERT INTO Mail (destinatario) VALUES ('sucorreo@dominio.com')
+
+INSERT INTO Producto(nombre,precio,tipo_producto,stock_minimo) VALUES('Granola Coco 200 grs',5500,'Granolas','30')
+INSERT INTO Producto(nombre,precio,tipo_producto,stock_minimo) VALUES('Granola Coco 400 grs',9500,'Granolas','25')
+INSERT INTO Producto(nombre,precio,tipo_producto,stock_minimo) VALUES('Granola Coco 1000 grs',14500,'Granolas','12')
+INSERT INTO Producto(nombre,precio,tipo_producto,stock_minimo) VALUES('Granola Vainilla 1000 grs',14000,'Granolas','12')
+INSERT INTO Producto(nombre,precio,tipo_producto,stock_minimo) VALUES('Granola Vainilla 250 grs',5500,'Granolas','30')
+INSERT INTO Producto(nombre,precio,tipo_producto,stock_minimo) VALUES('Granola Frutos Rojos 300 grs',8500,'Granolas','25')
